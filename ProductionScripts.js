333fee9d5dd10d2d03835b76f842e91cbc91c6f5
jasonbiola <140533085+jasonbiola@users.noreply.github.com>
2023-07-25 11:39:46 -0700

    Create ProductionScripts.js

---
+++ ProductionScripts.js
@@ -0,0 +1,23 @@
+var styles = `
+
+.DetailAction  { 
+  display: block;
+  font-weight: normal;
+  }
+
+a.DetailAction {
+  font-size: 18px;
+  }  
+
+a.DetailAction, button.DetailAction, div.DetailAction {
+  font-size: 18px;
+}
+
+h3.category-title {
+text-align: center;
+}
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
