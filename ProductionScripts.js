799ffa30e8f2fd4d99fd293f8b34b693fc7b863c
jasonbiola <140533085+jasonbiola@users.noreply.github.com>
2023-07-31 16:21:22 -0700

    Update ProductionScripts.js

--- ProductionScripts.js
+++ ProductionScripts.js
@@ -1,9 +1,11 @@
 var styles = `
 
+/* This makes the action button text not bold */
 .DetailAction  { 
   font-weight: normal;
   }
 
+/* This makes the action button text smaller and the text aligned */
 a.DetailAction {
   font-size: 18px;
   display: flex;
@@ -14,12 +16,14 @@ a.DetailAction, button.DetailAction, div.DetailAction {
   font-size: 18px;
   }
 
+/* This makes the action button icon centered vertically */
 .fa-fw {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
+/* This makes the category button icons smaller */
 .category-box .category-icon {
 font-size: 3em;
 margin-top: 16px;
