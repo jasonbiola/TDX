8c38143ec892d253654648f0310964d2a81bf713
jasonbiola <140533085+jasonbiola@users.noreply.github.com>
2023-08-01 02:43:36 -0700

    Update ProductionScripts.js

--- ProductionScripts.js
+++ ProductionScripts.js
@@ -8,17 +8,13 @@ var styles = `
 /* This makes the action button text smaller and the text aligned */
 a.DetailAction {
   font-size: 18px;
+  display: flex;
+  align-items: flex-start;
   }  
 
 a.DetailAction, button.DetailAction, div.DetailAction {
   font-size: 18px;
-  align-items: flex-start;
   }
-
-.fa {
-  
-}
-
 
 /* This makes the action button icon centered vertically - Need correct code for this */
 .fa-fw {
