5786fdd6fda013912193b4e933c94ea4cd969946
jasonbiola <140533085+jasonbiola@users.noreply.github.com>
2023-08-08 08:25:35 -0700

    Update SandboxScripts.js

--- SandboxScripts.js
+++ SandboxScripts.js
@@ -18,6 +18,33 @@ font-size: 3em;
 margin-top: 16px;
 }
 
+.rss-container {
+    background-color: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    font-family: Arial, sans-serif;
+  }
+
+  .rss-entry {
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  .rss-title {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .rss-description {
+    font-size: 14px;
+  }
+
+
 `
 
 var styleSheet = document.createElement("style")
