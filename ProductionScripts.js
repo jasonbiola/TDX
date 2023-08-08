e65cd6054307b7322f48528a5dcfa0bb462925aa
jasonbiola <140533085+jasonbiola@users.noreply.github.com>
2023-08-08 10:38:17 -0700

    Update ProductionScripts.js

--- ProductionScripts.js
+++ ProductionScripts.js
@@ -20,6 +20,18 @@ font-size: 3em;
 margin-top: 16px;
 }
 
+.rss-container {
+   padding: 15px;
+   font-family: Arial, sans-serif;
+  }
+
+ div.rss-headline {
+    color: #2268A0;
+    font-size: 1.2em;
+    font-weight: 400;
+    line-height: 16px;
+  }
+
 `
  
 var styleSheet = document.createElement("style")
