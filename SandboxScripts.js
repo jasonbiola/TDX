a018990288c6c75c6cbf731d92b855f3a7b5b314
Adam Snell <adam.snell@biola.edu>
2023-08-10 15:43:37 -0700

    Update SandboxScripts.js

--- SandboxScripts.js
+++ SandboxScripts.js
@@ -1,3 +1,4 @@
+/* We shouldn't need this style information anymore; all styling should be handled by the biola-stylesheet.css file, which is appended via the script below. But I didn't want to just delete all of this, and then miss it later.  -Adam Snell, 2023/08/10
 var styles = `.DetailAction  {  display: block; font-weight: normal;  }
 a.DetailAction {font-size: 18px;}  
 a.DetailAction, button.DetailAction, div.DetailAction {font-size: 18px;}
@@ -5,18 +6,27 @@ a.DetailAction, button.DetailAction, div.DetailAction {font-size: 18px;}
 .rss-container {padding: 10px;font-family: Arial, sans-serif;}
 div.rss-headline {color: #2268A0;font-size: 14px;font-weight: 500;line-height: 16px;}
 `
+*/
 
-//  Define where the stylesheet is stored
-var stylesheetUrl = `https://jasonbiola.github.io/TDX/biola-stylesheet.css`;
+/**
+* Appends the biola-stylesheet.css file to the page's head
+**/
+function appendStylesheet ()
+{
+  //  Define where the stylesheet is stored
+  var stylesheetUrl = `https://jasonbiola.github.io/TDX/biola-stylesheet.css`;
+  
+  //  Define a new "link" element to be appended to the document head
+  var stylesheetLink = document.createElement('link');
+  stylesheetLink.rel  = 'stylesheet';
+  stylesheetLink.type = 'text/css';
+  stylesheetLink.href = stylesheetUrl;
+  stylesheetLink.media = 'all';
+  
+  //  Get a handle on the document's head
+  var head = document.getElementsByTagName('head')[0];
+  //  Append the stylesheet link to the head
+  head.appendChild(stylesheetLink);
+}
 
-//  Define a new "link" element to be appended to the document head
-var stylesheetLink = document.createElement('link');
-stylesheetLink.rel  = 'stylesheet';
-stylesheetLink.type = 'text/css';
-stylesheetLink.href = stylesheetUrl;
-stylesheetLink.media = 'all';
-
-//  Get a handle on the document's head
-var head = document.getElementsByTagName('head')[0];
-//  Append the stylesheet link to the head
-head.appendChild(stylesheetLink);
+appendStylesheet ();
