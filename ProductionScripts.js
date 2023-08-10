f5aaca7d342372842edd2f8ab1fb0a785598f1ee
Adam Snell <adam.snell@biola.edu>
2023-08-10 15:47:08 -0700

    Update ProductionScripts.js
    Updated to now create a <link> to the biola-stylesheet.css file, rather than explicitly writing the styling to the header.

--- ProductionScripts.js
+++ ProductionScripts.js
@@ -1,55 +1,32 @@
-var styles = `
+/* We shouldn't need this style information anymore; all styling should be handled by the biola-stylesheet.css file, which is appended via the script below. But I didn't want to just delete all of this, and then miss it later.  -Adam Snell, 2023/08/10
+var styles = `.DetailAction  {  display: block; font-weight: normal;  }
+a.DetailAction {font-size: 18px;}  
+a.DetailAction, button.DetailAction, div.DetailAction {font-size: 18px;}
+.category-box .category-icon {font-size: 3em;margin-top: 16px;}
+.rss-container {padding: 10px;font-family: Arial, sans-serif;}
+div.rss-headline {color: #2268A0;font-size: 14px;font-weight: 500;line-height: 16px;}
+`
+*/
 
-/* This makes the action button text not bold */
-.DetailAction  { 
-  font-weight: normal;
-  }
-
-/* This makes the action button text smaller */
-a.DetailAction {
-  font-size: 18px;
-  }  
-
-a.DetailAction, button.DetailAction, div.DetailAction {
-  font-size: 18px;
-  }
-
-/* This makes the category button icons  smaller */
-.category-box .category-icon {
-font-size: 3em;
-margin-top: 16px;
+/**
+* Appends the biola-stylesheet.css file to the page's head
+**/
+function appendStylesheet ()
+{
+  //  Define where the stylesheet is stored
+  var stylesheetUrl = `https://jasonbiola.github.io/TDX/biola-stylesheet.css`;
+  
+  //  Define a new "link" element to be appended to the document head
+  var stylesheetLink = document.createElement('link');
+  stylesheetLink.rel  = 'stylesheet';
+  stylesheetLink.type = 'text/css';
+  stylesheetLink.href = stylesheetUrl;
+  stylesheetLink.media = 'all';
+  
+  //  Get a handle on the document's head
+  var head = document.getElementsByTagName('head')[0];
+  //  Append the stylesheet link to the head
+  head.appendChild(stylesheetLink);
 }
 
-div.rss-container {
-   padding: 15px;
-   font-family: Arial, sans-serif;
-  }
-
- div.rss-headline {
-    font-family: Helvetica Neue;
-    color: #2b2b2b;
-    font-size: 1.2em;
-    font-weight: 400;
-    line-height: 1.42857143;
-   }
-
-.quickjump-container {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  }
-  
-.quickjump {
-  margin: 0 10px;
-  text-align: center;
-  width: 200px;
-  }
-
-#32772 {
-  margin: 2em;
-  }
-`
- 
-var styleSheet = document.createElement("style")
-styleSheet.innerText = styles
-document.head.appendChild(styleSheet)
+appendStylesheet ();
