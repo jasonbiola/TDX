e8e27fbba2a52795e6e6d2a2750114122f48b577
Adam Snell <adam.snell@biola.edu>
2023-08-16 10:08:54 -0700

    Update ProductionScripts.js
    Added:
    - addFoundationJS ()
    - addFoundationCSS ()
    - fixSearchBars ()

--- ProductionScripts.js
+++ ProductionScripts.js
@@ -1,15 +1,40 @@
-/* We shouldn't need this style information anymore; all styling should be handled by the biola-stylesheet.css file, which is appended via the script below. But I didn't want to just delete all of this, and then miss it later.  -Adam Snell, 2023/08/10
-var styles = `.DetailAction  {  display: block; font-weight: normal;  }
-a.DetailAction {font-size: 18px;}  
-a.DetailAction, button.DetailAction, div.DetailAction {font-size: 18px;}
-.category-box .category-icon {font-size: 3em;margin-top: 16px;}
-.rss-container {padding: 10px;font-family: Arial, sans-serif;}
-div.rss-headline {color: #2268A0;font-size: 14px;font-weight: 500;line-height: 16px;}
-`
-*/
+/**
+* Appends the Foundation 6.7.5 javascript file to <body>,
+* and calls Foundation's initialization function
+**/
+
+function addFoundationJS () {
+    var script = document.createElement('script');
+    script.onload = function () {
+        $(document).foundation();
+    };
+    script.src = `https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/js/foundation.min.js`;
+    script.crossorigin = "anonymous";
+    document.body.appendChild(script);
+};
 
 /**
-* Appends the biola-stylesheet.css file to the page's head
+* Prepends the Foundation 6.7.5 CSS to <head>
+* (so that Bootstrap's CSS will take precedence for any "shared" classes)
+**/
+function addFoundationCSS ()
+{
+  //  Define the url of the Bootstrap CSS file
+  var stylesheetUrl = `https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css`;
+  //  Define a new "link" element to be appended to the document head
+  var cssTag = document.createElement ("link");
+  cssTag.crossorigin = "anonymous";
+  cssTag.rel  = "stylesheet";
+  cssTag.type = "text/css";
+  cssTag.href = stylesheetUrl;
+  //  Get a handle on the document's head
+  var head = document.getElementsByTagName ("head")[0];
+  //  Append the stylesheet link to the head
+  head.prepend(cssTag);
+}
+
+/**
+* Appends the biola-stylesheet.css file to <head>
 **/
 function appendStylesheet ()
 {
@@ -29,4 +54,33 @@ function appendStylesheet ()
   head.appendChild(stylesheetLink);
 }
 
+/**
+* Searches the #content div for any .site-search containers,
+* and restyles them to make them "more shiny"
+**/
+function fixSearchBars () {
+    $("#content .site-search").each (function () {
+        console.log (`Restyling in-page site search ${ $(this).attr("id") }`);
+        $(this)
+            .addClass("align-middle grid-x small-12")
+            .css({
+                "background-color":"#d9edf7",
+                "border":"1px solid #2268ab",
+                "border-radius":"10px",
+                "margin":"0",
+                "max-width":"100%",
+                "padding":"14px"
+            });
+        $(this).children("*").addClass ("cell");
+        $(this).find("input").addClass ("auto");
+    })
+}
+
 appendStylesheet ();
+addFoundationCSS ();
+addFoundationJS();
+
+//    Wait .5 seconds for the DOM to finish loading, before calling fixSearchBars
+var fsbTimeout = setTimeout(fixSearchBars, 500);
+//    This is just for visual confirmation of which version of the script is loading.
+console.log ("ProductionScripts.js version 2023.08.16");
