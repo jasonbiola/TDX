8830273f9198cb0722ab1fbec85c722c53e97cd8
Adam Snell <adam.snell@biola.edu>
2025-02-24 10:11:04 -0800

    Create Set searchbar placeholder.js

---
+++ Set searchbar placeholder.js
@@ -0,0 +1,6 @@
+<script>
+var placeholderText = `Your text here`;
+$(document).ready (function () {
+  $(`.ModuleContent [id*="SiteSearch-text"]`).attr("placeholder",placeholderText);
+});
+</script>
